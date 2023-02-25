b91e1a33d7e07f13a9044aa801efe3a7c0a336c6
eemottus <emottus@tktk.eegit config --global user.name eemottus git config --global user.email emottus@tktk.ee>
2023-02-25 10:39:23 +0200

    week7

---
+++ Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+static void PrintHello() //function /method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
+
+
+
